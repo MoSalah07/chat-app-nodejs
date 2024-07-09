d302713cb14b2ff9a08d331125f2da8a4d491fa0
MoSalah07 <notway10@yahoo.com>
2024-07-10 02:53:08 +0300

    Ready To Deploy

--- api/index.js
+++ api/index.js
@@ -6,7 +6,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const userController = require("./controller/user");
